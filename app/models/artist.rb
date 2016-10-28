6e3f16d402adb00f8cf3efe8b709b13260cece43
Juan Manuel Guzman Nava <leunam.toshi@gmail.com>
2016-10-28 16:53:52 -0500

    corrigiendo el error que crasheaba la app

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,7 +2,7 @@ class Artist < ActiveRecord::Base
 	has_attached_file :imagen, styles: { medium: "720x720>", thumb: "100x100>" }
 	validates_attachment_content_type :imagen, content_type: /\Aimage\/.*\z/
 	
-    :url  => ":s3_domain_url",,
+    :url  => ":s3_domain_url",
     :storage => :fog,
     :fog_credentials => {
         provider: 'AWS',

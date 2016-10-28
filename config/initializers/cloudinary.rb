4b04bcc16e0a2f8c08657f5eee3ece39de8fc0bd
Juan Manuel Guzman Nava <leunam.toshi@gmail.com>
2016-10-28 15:48:23 -0500

    1

--- config/initializers/cloudinary.rb
+++
@@ -1,6 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = 'toshiloco'
-  config.api_key = '892699781155175'
-  config.api_secret = '8wGYFQmCTkYWmYdheDulpXilKdA'
-  config.cdn_subdomain = true
-end
